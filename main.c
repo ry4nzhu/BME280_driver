5450d51386711c3f82dd4bcee8f7dadfe44930af
Ruiyang Zhu <ryanzhu@umich.edu>
2019-03-22 15:33:45 -0400

    test normal

--- main.c
+++ main.c
@@ -29,7 +29,6 @@ int8_t user_i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16
      * The parameter dev_id can be used as a variable to store the I2C address of the device
      */
     mss_i2c_status_t status;
-    //printf("dev id is: %u\r\n", dev_id);
 
     MSS_I2C_write(&g_mss_i2c1, BME280_I2C_ADDR_PRIM, &reg_addr, sizeof(reg_addr), MSS_I2C_RELEASE_BUS);
     MSS_I2C_wait_complete(&g_mss_i2c1, MSS_I2C_NO_TIMEOUT);
@@ -42,18 +41,18 @@ int8_t user_i2c_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16
 int8_t user_i2c_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, uint16_t len)
 {
 	int i = 0;
-    int8_t rslt = 0; /* Return 0 for Success, non-zero for failure */
-//    uint8_t* arr = malloc((len + 1) * sizeof(uint8_t));
-//    arr[0] = reg_addr;
-//    while (i < len) {
-//    	arr[i+1] = *(reg_data + i);
-//    	i++;
-//    }
-   uint8_t arr[2] = {reg_addr, *reg_data};
-
-    MSS_I2C_write(&g_mss_i2c1, BME280_I2C_ADDR_PRIM, arr, sizeof(arr), MSS_I2C_RELEASE_BUS);
-    MSS_I2C_wait_complete(&g_mss_i2c1, MSS_I2C_NO_TIMEOUT);
+	int8_t rslt = 0; /* Return 0 for Success, non-zero for failure */
+   // uint8_t* arr = malloc((len + 1) * sizeof(uint8_t));
+   // arr[0] = reg_addr;
+   // while (i < len) {
+   // 	arr[i+1] = *(reg_data + i);
+   // 	i++;
+   // }
     //free(arr);
+	uint8_t arr[2] = {reg_addr, *reg_data};
+
+	MSS_I2C_write(&g_mss_i2c1, BME280_I2C_ADDR_PRIM, arr, sizeof(arr), MSS_I2C_RELEASE_BUS);
+    MSS_I2C_wait_complete( &g_mss_i2c1, MSS_I2C_NO_TIMEOUT);
 
     return rslt;
 }
@@ -69,7 +68,6 @@ void print_sensor_data(struct bme280_data *comp_data)
 
 int main()
 {
-
 	struct bme280_dev dev;
 	struct bme280_data comp_data;
 	int8_t rslt = BME280_OK;
@@ -90,15 +88,19 @@ int main()
 		printf("success init!\r\n");
 	}
 
-	  /* Recommended mode of operation: Indoor navigation */
-	  dev.settings.osr_h = BME280_OVERSAMPLING_1X;
-	  dev.settings.osr_p = BME280_OVERSAMPLING_16X;
-	  dev.settings.osr_t = BME280_OVERSAMPLING_2X;
-	  dev.settings.filter = BME280_FILTER_COEFF_16;
+	/* Recommended mode of operation: Indoor navigation */
+	dev.settings.osr_h = BME280_OVERSAMPLING_1X;
+	dev.settings.osr_p = BME280_OVERSAMPLING_16X;
+	dev.settings.osr_t = BME280_OVERSAMPLING_2X;
+	dev.settings.filter = BME280_FILTER_COEFF_OFF;
+	dev.settings.standby_time = BME280_STANDBY_TIME_250_MS;
 
-	  settings_sel = BME280_OSR_PRESS_SEL | BME280_OSR_TEMP_SEL | BME280_OSR_HUM_SEL | BME280_FILTER_SEL;
+	settings_sel = BME280_ALL_SETTINGS_SEL;
 
-	  rslt = bme280_set_sensor_settings(settings_sel, &dev);
+	rslt = bme280_set_sensor_settings(settings_sel, &dev);
+
+	// set the sensor to normal mode
+	rslt = bme280_set_sensor_mode(BME280_NORMAL_MODE, &dev);
 
 //	if (bme280_set_sensor_mode(BME280_NORMAL_MODE, &dev) == 0) {
 //		printf("sensor mode normal\r\n");
@@ -107,7 +109,7 @@ int main()
 	  printf("Temperature, Pressure, Humidity\r\n");
 	  /* Continuously stream sensor data */
 	  while (1) {
-	    rslt = bme280_set_sensor_mode(BME280_FORCED_MODE, &dev);
+	    //rslt = bme280_set_sensor_mode(BME280_FORCED_MODE, &dev);
 	    /* Wait for the measurement to complete and print data @25Hz */
 	    dev.delay_ms(40);
 	    rslt = bme280_get_sensor_data(BME280_ALL, &comp_data, &dev);
